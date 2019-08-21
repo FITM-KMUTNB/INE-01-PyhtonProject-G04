1f54b9a19092a933de9584bdff0bbc5456a7d52b
Poowadol Rungtabnapa <51953419+PoowadolRungtabnapa@users.noreply.github.com>
2019-08-21 17:19:38 +0700

    ร่างโครง

--- Project Human Vs Alien/HumanVsAlien.py
+++ Project Human Vs Alien/HumanVsAlien.py
@@ -1,2 +1,18 @@
 import pgzrun
+import time
+from random import randint
 
+#Human =
+#Alien =
+
+def draw():
+    #เมนูเกม
+
+def drawCentretext(t):
+    #ตัวอักษร
+
+def Human():
+    #Player1
+
+def Alien():
+    #PLayer2
