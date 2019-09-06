ef63adbc866821fbd08e44ac79d9de5ad51391c4
Poowadol Rungtabnapa <51953419+PoowadolRungtabnapa@users.noreply.github.com>
2019-09-06 11:58:19 +0700

    Spaceship

--- Project Human Vs Alien/HumanVsAlien.py
+++ Project Human Vs Alien/HumanVsAlien.py
@@ -4,19 +4,16 @@ import time
 import re
 from random import randint
 
-#Human =
+Human = Actor('Space')
 #Alien =
 start = Actor('start-text')
 background = Actor('background')
 def draw():
     screen.blit('background', (0,0))
-    screen.blit('humanvsalientext', (0,0))
-    start.draw()
 
 
-def on_mouse_down(pos):
-    if start.collidepoint() :
-        screen.blit('background', (0,0))
+#def on_mouse_down(pos):
+   # screen.blit('background', (0,0))
 
 
 
