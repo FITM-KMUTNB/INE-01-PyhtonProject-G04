a2e79738f320de54e520add29a74202a35e24044
Poowadol Rungtabnapa <51953419+PoowadolRungtabnapa@users.noreply.github.com>
2019-09-08 12:30:22 +0700

    update

--- Project Human Vs Alien/HumanVsAlien.py
+++ Project Human Vs Alien/HumanVsAlien.py
@@ -4,22 +4,40 @@ import time
 import re
 from random import randint
 
-Human = Actor('Space')
-#Alien =
+Human = Actor('spaceship')
+Alien = Actor('alien1')
 start = Actor('start-text')
 background = Actor('background')
+
+Human.pos = 100,550
+Alien.pos = 750,50
+
 def draw():
     screen.blit('background', (0,0))
+    Human.draw()
+    Alien.draw()
 
+def on_mouse_down(pos):
+   if Alien.collidepoint(pos) :
+       place_Alien()
 
-#def on_mouse_down(pos):
-   # screen.blit('background', (0,0))
-
-
-
-#def Human():
+def update():
+    if keyboard.left :
+        Human.x -= 2
+    elif keyboard.right :
+        Human.x += 2
+    elif keyboard.up :
+        Human.y -= 2
+    elif keyboard.down :
+        Human.y += 2
 
 #def Alien():
 
+def place_Alien():
+    Alien.pos = 100,50
+    Alien.draw()
+
+#def AlienSkill () :
+#def init () :
 
 pgzrun.go()
